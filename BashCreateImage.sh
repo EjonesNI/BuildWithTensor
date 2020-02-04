b627a930d7783cbd5226404fe6477f00549e1fd7
EjonesNI <31165813+EjonesNI@users.noreply.github.com>
2020-02-04 23:13:56 +0000

    Cleaned up with git repo changes

--- BashCreateImage.sh
+++ BashCreateImage.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-dnf install -y gawk make wget tar qemu bzip2 gzip python unzip findutils which perl patch diffutils diffstat bc cpio file git cpp gcc gcc-c++ gnupg glibc-devel texinfo chrpath openssl ccache perl-Data-Dumper perl-bignum perl-Text-ParseWords perl-Thread-Queue SDL-devel xterm iputils hostname procps net-tools iproute python-pip python-devel python-requests redhat-rpm-config openssl-devel rpm-devel rsync tar tmux xz uhd
+dnf install -y gawk make wget tar bzip2 gzip python unzip findutils which perl patch diffutils diffstat bc cpio file git cpp gcc gcc-c++ gnupg glibc-devel texinfo chrpath openssl ccache perl-Data-Dumper perl-bignum perl-Text-ParseWords perl-Thread-Queue SDL-devel xterm iputils hostname procps net-tools iproute python-pip python-devel python-requests redhat-rpm-config openssl-devel rpm-devel rsync tar tmux xz uhd
 dnf clean all
 	
 echo "export LANG=en_US.utf-8" > /opt/export_LANG.sh
@@ -9,41 +9,22 @@ curl http://commondatastorage.googleapis.com/git-repo-downloads/repo > repo
 chmod a+x repo
 mv repo /usr/local/bin
 
-cd $HOME
-
-git clone -n git://github.com/openembedded/bitbake.git
-git checkout 79e62eef1c93f742bf71e9f25db57fdd2ffedd02
-git clone git://github.com/EttusResearch/meta-ettus.git
-git clone git://github.com/mendersoftware/meta-mender.git
-git clone git://github.com/openembedded/meta-openembedded.git
-git clone git://git.yoctoproject.org/meta-security.git
-git clone -b warrior git://github.com/openembedded/openembedded-core.git
-git clone git://git.yoctoproject.org/meta-qt4.git
-git clone git://github.com/EttusResearch/meta-sdr.git
-git clone git://git.yoctoproject.org/meta-tensorflow.git
-
-git clone https://github.com/EjonesNI/BuildWithTensor.git
-rm -f meta-ettus/contrib/setup_build_env.sh
-mv BuildWithTensor/setup_build_env.sh meta-ettus/contrib/setup_build_env.sh
-mv ./openembedded-core ./oe-core
-dos2unix meta-ettus/contrib/setup_build_env.sh
-
 mkdir sdk
 cd sdk
 wget http://files.ettus.com/e3xx_images/e3xx-release-4/oecore-x86_64-armv7ahf-vfp-neon-toolchain-nodistro.0.sh
 chmod +x oecore-x86_64-armv7ahf-vfp-neon-toolchain-nodistro.0.sh
-./oecore-x86_64-armv7ahf-vfp-neon-toolchain-nodistro.0.sh
-/usr/local/oecore-x86_64
-y
-ls
+./oecore-x86_64-armv7ahf-vfp-neon-toolchain-nodistro.0.sh -y -d /usr/local/oecore-x86_64
 source /usr/local/oecore-x86_64/environment-setup-armv7ahf-vfp-neon-oe-linux-gnueabi
 echo $CC
 
 cd $HOME
+repo init -u git://github.com/EJonesNI/oe-manifests.git -b Tensorflow
+repo sync
+
 chmod +x meta-ettus/contrib/setup_build_env.sh
-./meta-ettus/contrib/setup_build_env.sh e310_sg3
+./meta-ettus/contrib/setup_build_env.sh e310_sg3 v1.0.0.0
 
 touch build/conf/sanity.conf
 
 chmod +x meta-ettus/contrib/build_imgs_package.sh
-./meta-ettus/contrib/build_imgs_package.sh e310_sg3
+./meta-ettus/contrib/build_imgs_package.sh e310_sg3 v1.0.0.0
