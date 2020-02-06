7bcdfe890601ad939cda9028c3c81f4e329df58f
EjonesNI <31165813+EjonesNI@users.noreply.github.com>
2020-02-06 18:54:22 +0000

    Update BuildScript.sh

--- BuildScript.sh
+++ BuildScript.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-dnf install -y gawk make wget tar bzip2 gzip python unzip findutils which perl patch diffutils diffstat bc cpio file git cpp gcc gcc-c++ gnupg glibc-devel texinfo chrpath openssl ccache perl-Data-Dumper perl-bignum perl-Text-ParseWords perl-Thread-Queue SDL-devel xterm iputils hostname procps net-tools iproute python-pip python-devel python-requests redhat-rpm-config openssl-devel rpm-devel rsync tar tmux xz
+#dnf install -y gawk make wget tar bzip2 gzip python unzip findutils which perl patch diffutils diffstat bc cpio file git cpp gcc gcc-c++ gnupg glibc-devel texinfo chrpath openssl ccache perl-Data-Dumper perl-bignum perl-Text-ParseWords perl-Thread-Queue SDL-devel xterm iputils hostname procps net-tools iproute python-pip python-devel python-requests redhat-rpm-config openssl-devel rpm-devel rsync tar tmux xz
+
+mkdir ~/bin
+PATH=~/bin:$PATH
+curl https://storage.googleapis.com/git-repo-downloads/repo > ~/bin/repo
+chmod a+x ~/bin/repo
 
 cd $HOME
 repo init -u git://github.com/EttusResearch/oe-manifests.git -b thud
@@ -17,8 +22,8 @@ unzip thud.zip -d /root/
 chmod +x ./meta-ettus/contrib/build_imgs_package.sh
 chmod +x meta-ettus/contrib/setup_build_env.sh
 
-./meta-ettus/contrib/setup_build_env.sh e310_sg3
+#./meta-ettus/contrib/setup_build_env.sh e310_sg3
 
-touch build/conf/sanity.conf
+#touch build/conf/sanity.conf
 
-./meta-ettus/contrib/build_imgs_package.sh e310_sg3
+#./meta-ettus/contrib/build_imgs_package.sh e310_sg3
